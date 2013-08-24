e785cbc5b4db5f9d60284cdaca0873071cc6c5e9
Marcio Toze <marcio.toze@gmail.com>
2013-08-24 14:40:48 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Controleacademico::Application.config.secret_token = '01da90849f3d8f883ba35bba0df47c63492c286e7591b0ae1cbef48176c0e6d237406a424bb052d28036de4d1ba59b7759036b2a82d3709ce59386e541eb78e3'
