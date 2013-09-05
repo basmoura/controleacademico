130c361089d8c199ad44458beeed8dfc6dee171a
Breno Moura <basmoura@gmail.com>
2013-09-05 09:04:42 -0300

    criado o model matter

---
+++ app/models/matter.rb
@@ -0,0 +1,8 @@
+class Matter < ActiveRecord::Base
+  attr_accessible :credits, :title, :teacher
+
+  validates_presence_of :title, :credits
+  validates_length_of  :title, maximum: 80
+  validates_length_of  :teacher, maximum: 60
+  validates_numericality_of :credits, greater_than: 1, less_than: 12
+end
