8ff429e8b4034ea358b9ca5f6d59479681a6c11a
Breno Moura <basmoura@gmail.com>
2013-09-15 12:24:28 -0300

    adiciona validacoes e xexemplo de validacao personalizada

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,7 +2,9 @@ class Work < ActiveRecord::Base
   attr_accessible :matter_id, :title, :final_dt
 
   validates_presence_of :title, :final_dt, :matter_id
-
+  validates_length_of :title, maximum: 40
+  validates_length_of :final_dt, is: 10
+#  validate :final_dt_validation, before: [:create]
   belongs_to :matter
 
   self.per_page = 5
@@ -10,4 +12,11 @@ class Work < ActiveRecord::Base
   def final_dt
     read_attribute(:final_dt).strftime("%d/%m/%Y") unless read_attribute(:final_dt).nil?
   end
+
+#  private
+#  def final_dt_validation
+#    if DateTime.now.strftime("%d/%m/%Y") > final_dt
+#      errors.add :final_dt, ""
+#    end
+#  end
 end
