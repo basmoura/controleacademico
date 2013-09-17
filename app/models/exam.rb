5a27b25d252972c03a53426598e40c4b67a08baa
Breno Moura <basmoura@gmail.com>
2013-09-16 22:00:35 -0300

    adiciona relacionamento de provas com matérias e assuntos

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,5 +1,7 @@
 class Exam < ActiveRecord::Base
-  attr_accessible :dt_exam, :grade, :location, :matter_id
+  attr_accessible :dt_exam, :grade, :location, :matter_id, :subjects_attributes
 
   belongs_to :matter
+  has_many :subjects
+  accepts_nested_attributes_for :subjects
 end
